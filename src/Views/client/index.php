c2cb462ce12f4e298fd1ce04b26f932cc611becd
Nicolas Ribeiro <nikolas.sebastian.ribeiro@gmail.com>
2026-05-09 20:05:11 -0300

    cargado vista de cliente y controlador de cliente como ejemplo

---
+++ src/Views/client/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista Cliente</title>
+</head>
+
+<body>
+    <h1>Bienvenido al Sistema de Clientes</h1>
+    <p>Aquí puedes gestionar tus clientes.</p>
+    <p><?php echo $clientName; ?></p>
+</body>
+
+</html>
