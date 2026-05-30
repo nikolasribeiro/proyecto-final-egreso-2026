ca5aaa0e010eebbf68114391aeb7ed79f73db513
Nicolas Ribeiro <nikolas.sebastian.ribeiro@gmail.com>
2026-05-30 09:01:23 -0300

    division de monolito css en archivos mas manejables

--- src/Vistas/plantillas/admin.php
+++ src/Vistas/plantillas/admin.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>HC - <?php echo e($titulo_pagina) ?></title>
-    <link rel="stylesheet" href="/assets/css/dashboard/style.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 
 <body>
